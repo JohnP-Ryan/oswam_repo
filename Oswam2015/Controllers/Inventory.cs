9a588b622a22ebd3f2bb1fdfc0542362795279ac
JohnP-Ryan <jryan3544@gmail.com>
2017-02-28 01:13:37 -0500

    Inventory Tab Data Connection Nightly Progress

--- Oswam2015/Controllers/Inventory.cs
+++ Oswam2015/Controllers/Inventory.cs
@@ -16,42 +16,77 @@ namespace OSWAM.Controllers
 {
     public class InventoryController : Controller
     {
-        /*
-        private readonly OSWAM_DataContext _context;
 
-        public Inventory(OSWAM_DataContext context)
-        {
-            _context = context;
-        }
+        private OSWAM_DataEntities dataContext = new OSWAM_DataEntities();
 
-        public async Task<IActionResult> Index()
-        {
-
-            return View(await _context.Product.ToListAsync());
-        }
-        */
         public ActionResult Index()
         {
-            return View();
+            var data = dataContext.GetInventoryProducts(null, null, 0, 1, 0, 4);
+
+            if (TempData["FormDataArray"] == null)
+            {
+                data = dataContext.GetInventoryProducts(null, null, 0, 1, 0, 200);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Data preserved: ");
+                String[] formData = (String[]) TempData["FormDataArray"];
+
+                for(int i = 0; i > 6; i++)
+                {
+                    System.Diagnostics.Debug.WriteLine("Temp: " + formData[i]);
+                }
+
+                data = dataContext.GetInventoryProducts(formData[0], formData[1], Int32.Parse(formData[2]), Int32.Parse(formData[3]), Int32.Parse(formData[4]), Int32.Parse(formData[5]));
+            }
+
+
+            return View(data.ToList());
         }
 
         [HttpPost]
-        public ActionResult Report(string textboxID, string textboxName, int weightLow, int weightHigh, int priceLow, int priceHigh)
+        public ActionResult Report(string textboxID = null, string textboxName = null, int weightLow = 0, int weightHigh = 0, int priceLow = 0, int priceHigh = 0)
         {
-            System.Diagnostics.Debug.WriteLine(textboxID);
-            System.Diagnostics.Debug.WriteLine(textboxName);
-            System.Diagnostics.Debug.WriteLine(weightLow);
-            System.Diagnostics.Debug.WriteLine(weightHigh);
-            System.Diagnostics.Debug.WriteLine(priceLow);
-            System.Diagnostics.Debug.WriteLine(priceHigh);
+            /*
+            System.Diagnostics.Debug.WriteLine("RUNNING");
+
+            
+
+            System.Diagnostics.Debug.WriteLine(fData.getFormId());
+            System.Diagnostics.Debug.WriteLine(fData.getFormName());
+            System.Diagnostics.Debug.WriteLine(fData.getFormWeightLow());
+            System.Diagnostics.Debug.WriteLine(fData.getFormWeightHigh());
+            System.Diagnostics.Debug.WriteLine(fData.getFormPriceLow());
+            System.Diagnostics.Debug.WriteLine(fData.getFormPriceHigh());
 
 
+            InventoryFormData fData = new InventoryFormData(null, null, 0, 1, 0, 4);
 
+            fData.setFormId(textboxID);
+            fData.setFormName(textboxName);
+            fData.setFormWeightLow(weightLow);
+            fData.setFormWeightHigh(weightHigh);
+            fData.setFormPriceLow(priceLow);
+            fData.setFormPriceHigh(priceHigh);
 
+            var data = dataContext.GetInventoryProducts(fData.getFormId(), fData.getFormName(), fData.getFormWeightLow(), fData.getFormWeightHigh(), fData.getFormPriceLow(), fData.getFormPriceHigh());
+            System.Diagnostics.Debug.WriteLine(fData.getFormId());
+            System.Diagnostics.Debug.WriteLine(fData.getFormName());
+            System.Diagnostics.Debug.WriteLine(fData.getFormWeightLow());
+            System.Diagnostics.Debug.WriteLine(fData.getFormWeightHigh());
+            System.Diagnostics.Debug.WriteLine(fData.getFormPriceLow());
+            System.Diagnostics.Debug.WriteLine(fData.getFormPriceHigh());
+            */
 
+            String[] formData = new String[6];
+            formData[0] = textboxID;
+            formData[1] = textboxName;
+            formData[2] = weightLow.ToString();
+            formData[3] = weightHigh.ToString();
+            formData[4] = priceLow.ToString();
+            formData[5] = priceHigh.ToString();
 
-
-
+            TempData["FormDataArray"] = formData;
 
             return RedirectToAction("Index");
         }
