9c8856fc9de272306a3344b4c730dababf21fe2f
JohnP-Ryan <Jryan3544@gmail.com>
2017-03-18 23:19:17 -0400

    Utility Controller and Views

---
+++ Oswam2015/Controllers/Utility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oswam2015.Controllers
+{
+    public class UtilityController : Controller
+    {
+        // GET: Utility
+        public ActionResult Index() //about page
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View("~/Views/Utility/About.cshtml");
+        }
+
+        public ActionResult Terms()
+        {
+            return View("~/Views/Utility/Terms.cshtml");
+        }
+
+        public ActionResult Contact()
+        {
+            return View("~/Views/Utility/Contact.cshtml");
+        }
+    }
+}
